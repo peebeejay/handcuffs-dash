e31cdd8df0d9ca347068486a9c1060952e07746b
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-02-28 18:46:46 -0500

    add moon icon

---
+++ src/components/icons/Moon.tsx
@@ -0,0 +1,15 @@
+import React, { forwardRef, Ref, SVGAttributes } from 'react';
+
+export const MoonIcon = forwardRef(
+  (props: SVGAttributes<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+    <svg
+      width={20}
+      height={20}
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M9.38.019a7.5 7.5 0 1010.6 10.6C19.662 15.854 15.316 20 10.001 20 4.477 20 0 15.523 0 10 0 4.685 4.146.339 9.38.019z" />
+    </svg>
+  ),
+);
