c9d39ad52270827382f996bdc00b8ad1ed6d3acd
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-02-28 18:46:58 -0500

    add colors

---
+++ src/colors.ts
@@ -0,0 +1,9 @@
+export const Gray1 = '#F1F6FE';
+export const Gray2 = '#E3EBF7';
+export const Gray3 = '#d0dae8';
+export const Gray4 = '#ACBED5';
+
+export const TextDark = '#0B1E58';
+
+export const PrimaryBlue = '#3366FF';
+export const PrimaryBlueDark = '#174BE6';
