b44afeb4084ef8903fdc147e09db278a14bfcafe
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-02-28 18:47:34 -0500

    integrate router & pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
+import { rem } from 'polished';
 import { Creation } from './pages/Creation';
 import { Detail } from './pages/Detail';
+import { Landing } from './pages/Landing';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import './App.css';
+import { Navbar } from './components/Navbar';
+
+const Container = styled.div`
+  background-color: white;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+`;
+
+const PageContainer = styled.div`
+  margin: ${rem(10)};
+`;
 
 export const App = () => {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <p>
-            <code>Handcuffs Scaffolding</code>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <ul>
-            <li>
-              <Link to="/">Creation</Link>
-            </li>
-            <li>
-              <Link to="/lock/5">Sample Detail Page</Link>
-            </li>
-          </ul>
-        </header>
+      <Container>
+        <Navbar />
+        <PageContainer>
+          <Switch>
+            <Route exact path="/">
+              <Landing />
+            </Route>
+            <Route exact path="/create">
+              <Creation />
+            </Route>
+            <Route exact path="/lock/:handcuffId">
+              <Detail />
+            </Route>
+          </Switch>
+        </PageContainer>
 
-        <Switch>
-          <Route exact path="/">
-            <Creation />
-          </Route>
-          <Route exact path="/lock/:handcuffId">
-            <Detail />
-          </Route>
-        </Switch>
-      </div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/create">Creation</Link>
+          </li>
+          <li>
+            <Link to="/lock/5">Sample Detail Page</Link>
+          </li>
+        </ul>
+      </Container>
     </Router>
   );
 };
