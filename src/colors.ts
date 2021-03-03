582465ebc7f066d8a520403846b23340f5b2f256
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-03-03 00:29:37 -0500

    add input colors

--- src/colors.ts
+++ src/colors.ts
@@ -3,7 +3,12 @@ export const Gray2 = '#E3EBF7';
 export const Gray3 = '#d0dae8';
 export const Gray4 = '#ACBED5';
 
+export const TextLight = '#5D71A2';
 export const TextDark = '#0B1E58';
 
 export const PrimaryBlue = '#3366FF';
 export const PrimaryBlueDark = '#174BE6';
+
+export const InputBlue = '#339DFF';
+export const InputOrange = '#FEA655';
+export const InputPurple = '#916BFD';
