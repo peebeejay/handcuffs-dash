c15ea25b373b09c2cfae8aeed893389499e97959
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-04-12 16:47:48 -0400

    swap out landing page components with shared + typography components

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,17 +1,8 @@
-import React from 'react';
 import styled from 'styled-components';
 import { rem } from 'polished';
-import { TextDark, PrimaryBlue, PrimaryBlueDark } from '../colors';
-import { Link } from 'react-router-dom';
-
-const LandingHeader = styled.div`
-  font-weight: 700;
-  font-size: ${rem(42)};
-  margin-top: ${rem(100)};
-  color: ${TextDark};
-  max-width: ${rem(550)};
-  text-align: center;
-`;
+import { Link } from '../components/shared/Link';
+import { LargeHeader } from '../components/typography/LargeHeader';
+import { Button } from '../components/shared/Button';
 
 const Container = styled.div`
   display: flex;
@@ -20,40 +11,23 @@ const Container = styled.div`
   align-items: center;
 `;
 
-const CreateVaultButton = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  height: ${rem(24)};
-  background-color: ${PrimaryBlue};
-  padding: ${rem(16)} ${rem(24)};
-  border-radius: ${rem(8)};
-
-  transition: 250ms background-color ease;
-  cursor: pointer;
-  margin-top: ${rem(24)};
-
-  &:hover {
-    background-color: ${PrimaryBlueDark};
-  }
+const LandingHeader = styled(LargeHeader)`
+  margin-top: ${rem(100)};
+  max-width: ${rem(550)};
+  text-align: center;
 `;
 
-const CreateLink = styled(Link)`
-  font-weight: 500;
-  font-size: 16px;
-  text-align: center;
-  color: white;
-  text-decoration: none;
+const CreateVaultButton = styled(Button)`
+  margin-top: ${rem(24)};
 `;
 
 export const Landing = () => {
   return (
     <Container>
       <LandingHeader>{'Hey, protect your tokens with social recovery.'}</LandingHeader>
-      <CreateVaultButton>
-        <CreateLink to="/create">Create Vault</CreateLink>
-      </CreateVaultButton>
+      <Link to="/create">
+        <CreateVaultButton>{'Create Vault'}</CreateVaultButton>
+      </Link>
     </Container>
   );
 };
