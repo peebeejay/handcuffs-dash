7f44342c6e621400b27553280a5da306f1567d90
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-04-12 16:49:06 -0400

    Create navigation button component for form

---
+++ src/components/CreationForm/Navigation.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import { rem } from 'polished';
+import { Button } from '../shared/Button';
+
+type Props = {
+  onClickBack: React.MouseEventHandler;
+  onClickContinue: React.MouseEventHandler;
+};
+
+const NavigationContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const BackButton = styled(Button)`
+  flex: 1;
+  margin-right: ${rem(15)};
+`;
+
+const ContinueButton = styled(Button)`
+  flex: 1;
+  margin-left: ${rem(15)};
+`;
+
+export const Navigation = (props: Props) => {
+  const { onClickBack, onClickContinue } = props;
+
+  return (
+    <>
+      <NavigationContainer>
+        <BackButton onClick={onClickBack}>{'Back'}</BackButton>
+        <ContinueButton onClick={onClickContinue}>{'Continue'}</ContinueButton>
+      </NavigationContainer>
+    </>
+  );
+};
