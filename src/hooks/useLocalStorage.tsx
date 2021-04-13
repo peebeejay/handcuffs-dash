429452f96436f67ba0bcad2edbd04629afd9aeca
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-04-12 20:26:05 -0400

    add useLocalStorage hook

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,33 @@
+import { useCallback, useState } from 'react';
+
+export function useLocalStorage<T = unknown>(key: string, initialValue: T) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      return initialValue;
+    }
+  });
+
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = useCallback(
+    (value: T) => {
+      try {
+        // Allow value to be a function so we have same API as useState
+        const valueToStore = value instanceof Function ? value(storedValue) : value;
+        // Save state
+        setStoredValue(valueToStore);
+        // Save to local storage
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      } catch (error) {}
+    },
+    [storedValue, setStoredValue, key],
+  );
+
+  return [storedValue, setValue];
+}
