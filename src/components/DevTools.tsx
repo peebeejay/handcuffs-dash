f0f1a083171ecd661ce557ec0999f9476ecce1b7
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-04-12 20:29:33 -0400

    store last position of dev tools in local storage

--- src/components/DevTools.tsx
+++ src/components/DevTools.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import { rem } from 'polished';
-import { useDrag } from 'react-use-gesture';
-import { Gray1, Gray2 } from '../colors';
 import { Link } from 'react-router-dom';
+import { useDrag } from 'react-use-gesture';
+import { Gray1, Gray2, Gray3 } from '../colors';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 
 type Position = {
   x: number;
@@ -30,22 +31,34 @@ const NavHelper = styled.div.attrs(({ x, y }: Position) => ({
   &:hover {
     background-color: ${Gray2};
   }
+
+  &:active {
+    background-color: ${Gray3};
+  }
 `;
 
 export const DevTools = () => {
-  // tx - The amount by which the component should be translated or offset on the x-axis
-  const [tx, setXOffset] = useState<number>(50);
-  // ty - The amount by which the component should be translated or offset on the y-axis
-  const [ty, setYOffset] = useState<number>(50);
+  /* Store dev tools position in local storage */
+  const [lsx, setLsx] = useLocalStorage<number>('hc-devtools-pos-x', 50);
+  const [lsy, setLsy] = useLocalStorage<number>('hc-devtools-pos-y', 50);
+  /* tx - The amount by which the component should be translated or offset on the x-axis */
+  const [tx, setXOffset] = useState<number>(lsx);
+  /* ty - The amount by which the component should be translated or offset on the y-axis */
+  const [ty, setYOffset] = useState<number>(lsy);
 
-  const bind = useDrag(({ delta: [dx, dy] }) => {
+  const bind = useDrag(({ last, delta: [dx, dy] }) => {
     setXOffset(tx + dx);
     setYOffset(ty + dy);
+
+    if (last) {
+      setLsx(tx);
+      setLsy(ty);
+    }
   });
 
   return (
     <NavHelper {...bind()} x={tx} y={ty}>
-      {'Dev Toolz '}
+      {'⚡️~Dev Toolz~🤓'}
       <ul>
         <li>
           <Link to="/">Home</Link>
