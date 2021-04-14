3932c20808d06af030781f82845d20c62cb1f1fa
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-04-14 19:27:48 -0400

    add new ExternalLink component

--- src/components/shared/Link.tsx
+++ src/components/shared/Link.tsx
@@ -1,6 +1,27 @@
 import styled from 'styled-components';
 import { Link as RouterLink } from 'react-router-dom';
+import { TextDark } from '../../colors';
 
 export const Link = styled(RouterLink)`
   text-decoration: none;
+`;
+
+export const ExternalLink = styled.a`
+  text-decoration: none;
+  cursor: pointer;
+  color: ${TextDark};
+  font-weight: 500;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  &:focus {
+    outline: none;
+    text-decoration: underline;
+  }
+
+  &:active {
+    text-decoration: none;
+  }
 `;
