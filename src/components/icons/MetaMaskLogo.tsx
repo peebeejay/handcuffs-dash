244787eeb0e94982f6c28f8f6dad848b9671c00d
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-04-14 19:28:09 -0400

    add react svg version of metamask logo

---
+++ src/components/icons/MetaMaskLogo.tsx
@@ -0,0 +1,189 @@
+import { forwardRef, Ref, SVGAttributes } from 'react';
+
+export const MetamaskLogo = forwardRef(
+  (props: SVGAttributes<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+    <svg
+      width={45}
+      height={41}
+      viewBox="0 0 45 41"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12.874 36.973l5.896 1.566v-2.047l.482-.482h3.369v4.095h-3.61l-4.452-1.927-1.684-1.205z"
+        fill="#CDBDB2"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M32.126 36.973L26.35 38.54v-2.047l-.481-.482h-3.37v4.095h3.61l4.452-1.927 1.565-1.205z"
+        fill="#CDBDB2"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M19.251 32.517l-.481 3.975.602-.482h6.256l.722.482-.481-3.975-.963-.602-4.813.12-.842.482z"
+        fill="#393939"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M16.003 5.66l2.888 6.745 1.323 19.63h4.693l1.444-19.63 2.647-6.745H16.003z"
+        fill="#F89C35"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M3.369 20.474L0 30.229l8.422-.482h5.415v-4.215l-.24-8.671-1.204.963-9.024 2.65z"
+        fill="#F89D35"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9.746 21.558l9.866.24-1.082 5.059-4.693-1.204-4.09-4.095z"
+        fill="#D87C30"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9.746 21.678l4.091 3.854v3.854l-4.09-7.708z"
+        fill="#EA8D3A"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M13.837 25.652l4.813 1.205 1.564 5.179-1.083.602-5.294-3.132v-3.854z"
+        fill="#F89D35"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M13.837 29.506l-.963 7.467 6.377-4.456-5.414-3.01z"
+        fill="#EB8F35"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M19.612 21.799l.602 10.237-1.805-5.24 1.203-4.997z"
+        fill="#EA8E3A"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8.302 29.627l5.535-.12-.963 7.466-4.572-7.346z"
+        fill="#D87C30"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2.647 40.225l10.227-3.252-4.572-7.346L0 30.229l2.647 9.996z"
+        fill="#EB8F35"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M18.89 12.405l-5.173 4.335-3.97 4.818 9.865.361-.721-9.514z"
+        fill="#E8821E"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12.874 36.973l6.377-4.456-.48 3.854v2.168l-4.332-.843-1.565-.723zM32.126 36.973l-6.257-4.456.481 3.854v2.168l4.332-.843 1.444-.723z"
+        fill="#DFCEC3"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M16.965 23.966l1.324 2.77-4.693-1.204 3.369-1.566z"
+        fill="#393939"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2.527 0L18.89 12.405 16.122 5.66 2.527 0z"
+        fill="#E88F35"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2.527 0L.36 6.624l1.203 7.226-.842.482 1.203 1.084-.963.843 1.324 1.204-.842.723 1.925 2.408 9.024-2.77c4.412-3.532 6.578-5.339 6.497-5.42C18.81 12.325 13.355 8.19 2.527 0z"
+        fill="#8E5A30"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M41.631 20.474L45 30.229l-8.422-.482h-5.415v-4.215l.24-8.671 1.204.963 9.024 2.65z"
+        fill="#F89D35"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M35.254 21.558l-9.866.24 1.082 5.059 4.693-1.204 4.09-4.095z"
+        fill="#D87C30"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M35.254 21.678l-4.091 3.854v3.854l4.09-7.708z"
+        fill="#EA8D3A"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M31.163 25.652l-4.813 1.205-1.564 5.179 1.083.602 5.294-3.132v-3.854z"
+        fill="#F89D35"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M31.163 29.506l.963 7.467-6.257-4.335 5.294-3.131z"
+        fill="#EB8F35"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M25.388 21.799l-.602 10.237 1.805-5.24-1.203-4.997z"
+        fill="#EA8E3A"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M36.698 29.627l-5.535-.12.963 7.466 4.572-7.346z"
+        fill="#D87C30"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M42.353 40.225l-10.227-3.252 4.572-7.346 8.302.602-2.647 9.996z"
+        fill="#EB8F35"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M26.11 12.405l5.173 4.335 3.97 4.818-9.865.361.721-9.514z"
+        fill="#E8821E"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M28.035 23.966l-1.324 2.77 4.693-1.204-3.369-1.566z"
+        fill="#393939"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M42.473 0L26.11 12.405l2.767-6.745L42.473 0z"
+        fill="#E88F35"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M42.473 0l2.166 6.624-1.203 7.226.842.482-1.203 1.084.963.843-1.324 1.204.842.723-1.925 2.408-9.024-2.77c-4.412-3.532-6.578-5.339-6.497-5.42.08-.08 5.535-4.214 16.363-12.404z"
+        fill="#8E5A30"
+      />
+    </svg>
+  ),
+);
