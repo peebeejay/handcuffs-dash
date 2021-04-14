9b32ae203f8d3ee342b6e3ede422815187b7f384
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-04-14 19:13:07 -0400

    add useEagerConnect hook

---
+++ src/hooks/useEagerConnect.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { injected } from '../connectors';
+import { useWeb3React } from '@web3-react/core';
+
+/**
+ * This hook attempts to connect to an injected provider upon page load to prevent
+ * a user from having to reconnect / login after every page refresh. *
+ */
+export function useEagerConnect() {
+  const { activate, active } = useWeb3React();
+  const [hasTried, setHasTried] = useState(false);
+
+  /* Only run this hook once ideally */
+  useEffect(() => {
+    injected.isAuthorized().then((isAuthorized) => {
+      if (isAuthorized) {
+        activate(injected, undefined, true).catch(() => {
+          setHasTried(true);
+        });
+      } else {
+        setHasTried(true);
+      }
+    });
+  }, [activate]);
+
+  /**
+   * If the connection worked, wait until a confirmation is received to
+   * toggle the hasTried flag.
+   */
+  useEffect(() => {
+    if (active) {
+      setHasTried(true);
+    }
+  }, [active]);
+
+  return hasTried;
+}
