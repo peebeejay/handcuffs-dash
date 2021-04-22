d97c6b1232d9265a9f971c40587ad58e22778e89
Jay Puntham-Baker <jay.puntham-baker@vimeo.com>
2021-04-21 23:01:20 -0400

    add margin to the form divider

--- src/components/shared/Divider.tsx
+++ src/components/shared/Divider.tsx
@@ -5,4 +5,6 @@ import { Gray3 } from '../../colors';
 export const Divider = styled.div`
   border: ${rem(1)} solid ${Gray3};
   width: ${rem(370)};
+  margin-top: ${rem(16)};
+  margin-bottom: ${rem(16)};
 `;
